d00d9c33c1e64696eb510dc8f56634fe69a0812e
ayush kumar <socialacc211@gmail.com>
2024-09-24 00:37:32 +0530

    added style using tailwindcss

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-
+import './index.css';
 
 
 
@@ -13,25 +13,33 @@ function App() {
 
 function counterUp() {
   counter +=1
+  //? state as a snapshot example && prevValue callback to eval the specified setVal update in-addition to last-update(defaul) explained
   setCounter(counter)
+  setCounter(counter);
+  setCounter(prevCounter => prevCounter+1);
+  setCounter( prevCounter => prevCounter+1);
   
 }
 
 function counterDown() {
   counter -=1
   setCounter(counter);
+
+  
 }
   
   var [counter, setCounter ] = useState(0);
   return (
   <>
-    <div>
-      <h1>Counter App</h1>
-      <div >
-          {counter}
-      </div>
-      <button onClick={counterUp}>increase</button>
-      <button onClick={counterDown}>decrease</button>
+    <div >
+      <div className="bg-slate-700 w-screen h-screen flex justify-center items-center flex-col gap-4">
+        <h1 className="text-3xl text-white " >Counter App</h1>
+        <div className="bg-gradient-to-br from-teal-500 to-red-400 w-24 p-5 text-white text-3xl  text-center rounded-lg" >
+            {counter}
+        </div>
+        <button className="bg-blue-600 px-4 py-2 border-none rounded-lg font-bold text-white text-2xl" onClick={counterUp}>increase</button>
+        <button className="bg-blue-600 px-4 py-2 border-none rounded-lg font-bold text-white text-2xl" onClick={counterDown}>decrease</button>
+      </div>  
     </div>
   </>
   )
